d208c23f6dd1feffddb2a80b387841292900aac0
Oscar <osm1019@gmail.com>
2024-05-25 20:02:16 -0700

    fix up rro_overlays

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -14,14 +14,51 @@
 # limitations under the License.
 #
 
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    AlphaConfigOverlay \
+    PixelLauncherIconsOverlay
 
 # Google Overlays
 PRODUCT_PACKAGES += \
+    AccessibilityMenuOverlay \
+    BuiltInPrintServiceOverlay \
+    CaptivePortalLoginOverlay \
+    CellBroadcastReceiverOverlay \
+    CellBroadcastServiceOverlay \
+    ClearCallingSettingsOverlay2022 \
+    ContactsProviderOverlay \
+    DMServiceOverlay \
+    FlipendoOverlay \
+    GoogleConfigOverlay \
+    GoogleDeviceLockControllerOverlay \
+    GoogleHealthConnectOverlay \
+    GoogleWebViewOverlay \
+    MediaProviderOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlayCommon \
+    PixelDocumentsUIGoogleOverlay \
+    PixelSetupWizardOverlay \
+    SettingsGoogleOverlayPixel2022 \
+    SettingsProviderOverlay \
+    SystemUIGoogleOverlay \
+    TeleServiceOverlay \
+    TelecomOverlay \
+    TelephonyProviderOverlay \
+    WildlifeSettingsVpnOverlay2022 \
+    WildlifeSysuiVpnOverlay2022 \
     ClearCallingSettingsOverlay2022 \
     CarrierSettingsConfigOverlay \
     CarrierSettingsOverlay \
     CarrierSettingsProviderOverlay \
-    CarrierSettingsSystemUIOverlay \
+    ClearCallingSettingsOverlay2022 \
+    ManagedProvisioningPixelOverlay \
     NowPlayingOverlay \
-
-
+    PixelBatteryHealthOverlay \
+    PixelDisableLEDOverlay \
+    PixelSetupWizardOverlay2019 \
+    PixelSetupWizardOverlay2021 \
+    PixelConnectivityOverlay2022 \
+    DreamlinerOverlay
