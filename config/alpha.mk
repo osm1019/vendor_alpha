10013fb613ab4fc442cc859d5af7d2e78b397567
Oscar <osm1019@gmail.com>
2024-06-04 20:26:45 -0700

    remove some aosp apps and edit gms call

--- config/alpha.mk
+++ config/alpha.mk
@@ -20,10 +20,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # 1 - vanilla (default)
 # 2 - microg
 # 3 - gapps
-ifeq ($(TARGET_BUILD_PACKAGE),3)
+
   BUILD_GMS_OVERLAYS_AND_PROPS := true
   $(call inherit-product, vendor/gms/gms_full.mk)
-else
-  ifeq ($(TARGET_BUILD_PACKAGE),2)
-    $(call inherit-product, vendor/microg/product.mk)
-  endif
+
+
--- config/common_full.mk
+++ config/common_full.mk
@@ -5,21 +5,21 @@ PRODUCT_SIZE := full
 
 # Apps
 PRODUCT_PACKAGES += \
-    Eleven \
-    Etar \
-    Profiles \
-    Recorder \
-    Seedvault
+  #  Eleven \
+   # Etar \
+   # Profiles \
+   # Recorder \
+   # Seedvault
 
-ifneq ($(PRODUCT_NO_CAMERA),true)
-PRODUCT_PACKAGES += \
-    Aperture
-endif
+#ifneq ($(PRODUCT_NO_CAMERA),true)
+#PRODUCT_PACKAGES += \
+ #   Aperture
+#endif
 
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
+#ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
+#PRODUCT_PACKAGES += \
+#    AudioFX
+#endif
 
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
