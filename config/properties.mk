59835401826ae8c0238b1f56466c43bea60fb5ca
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 07:58:33 +0800

    Disable memtag for different processes
    * it defaults to async if not specified
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/properties.mk
+++ config/properties.mk
@@ -62,9 +62,12 @@ ifeq ($(TARGET_ENABLE_BLUR), true)
     ro.surface_flinger.supports_background_blur=1
 endif
 
-# Disable async MTE on system_server
+# Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    arm64.memtag.process.system_server=off
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
 
 # Enable dex2oat64 to do dexopt
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
